0fad071684b7949ab2777e67685df7ac935d12a7
Jaeden <jaedenjwest@gmail.com>
2022-05-24 21:34:42 +1200

    Add root href

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -11,16 +11,18 @@ function Navigation(props) {
 	return (
 		<nav className="relative w-full flex flex-wrap items-center justify-between py-3.5 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-md">
 			<div className="container-fluid w-full flex flex-wrap items-center px-3">
-				<div className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mr-1">
-					<img
-						className="inline-flex w-9 mx-2"
-						src={Pokeball}
-						alt="Pokeball"
-					/>
-					<h1 className="inline-flex font-medium text-xl mr-2">
-						Pokédex
-					</h1>
-				</div>
+				<a href="/">
+					<div className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mr-1">
+						<img
+							className="inline-flex w-9 mx-2"
+							src={Pokeball}
+							alt="Pokeball"
+						/>
+						<h1 className="inline-flex font-medium text-xl mr-2">
+							Pokédex
+						</h1>
+					</div>
+				</a>
 				<input
 					type="text"
 					id="search-navbar"
