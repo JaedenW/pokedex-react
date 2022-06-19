781d8f96db591afa3a53032f1cd6c47418068e55
Jaeden <jaedenjwest@gmail.com>
2022-06-19 22:58:59 +1200

    Minor style tweaks on moves

--- src/Components/Move.js
+++ src/Components/Move.js
@@ -11,73 +11,72 @@ function Move({ move, moveDetails }) {
   return (
     <div
       className={`${
-        wasClicked ||
-        'sm:hover:scale-105 sm:hover:cursor-pointer sm:hover:shadow-lg'
-      } m-2 h-fit w-[90%] rounded-md p-2 shadow-md transition sm:w-[45%] xl:w-[30%]`}
+        wasClicked || 'sm:hover:scale-105 sm:hover:shadow-lg'
+      } m-2 h-fit w-[90%] rounded-md p-2 shadow-md transition sm:w-[45%] sm:cursor-pointer xl:w-[30%]`}
       style={{ backgroundColor: typeColours[moveData.type.name] }}
       onClick={() => setWasClicked((prevState) => !prevState)}
     >
       <h3 className="mb-1 whitespace-nowrap text-lg text-gray-100">
         {getDisplayName(move.name).toUpperCase()}
       </h3>
-      <div
+      <ul
         className={`${
           wasClicked ? 'visible' : 'hidden'
-        } rounded-md bg-gray-100 p-2 sm:text-sm`}
+        } divide-y divide-stone-300 rounded-md bg-gray-100 p-2 sm:text-sm`}
       >
-        <div className="my-2 flex justify-between">
+        <li className="my-2 flex justify-between">
           <p className="inline-flex max-w-[60%] text-left">Type:</p>
           <div>
             <Type type={moveData.type} />
           </div>
-        </div>
+        </li>
         {moveDetails.level_learned_at > 0 ? (
-          <div className="flex justify-between">
-            <p className="inline-flex max-w-[55%] text-left">Learned at:</p>
+          <li className="flex justify-between">
+            <p className="inline-flex max-w-[50%] text-left">Learned at:</p>
             <p className="inline text-right font-normal">
               Level {moveDetails.level_learned_at}
             </p>
-          </div>
+          </li>
         ) : (
-          <div className="flex justify-between">
-            <p className="inline-flex max-w-[60%] text-left">Learned via:</p>
+          <li className="flex justify-between">
+            <p className="inline-flex max-w-[50%] text-left">Learned via:</p>
             <p className="inline text-right font-normal">TM or HM</p>
-          </div>
+          </li>
         )}
-        <div className="flex justify-between">
-          <p className="inline-flex max-w-[60%] text-left">PP:</p>
+        <li className="flex justify-between">
+          <p className="inline-flex max-w-[50%] text-left">PP:</p>
           <p className="inline-flex text-right font-normal">{moveData.pp}</p>
-        </div>
-        <div className="flex justify-between">
-          <p className="inline-flex max-w-[60%] text-left">Target:</p>
-          <p className="inline-flex max-w-[40%] text-right font-normal">
+        </li>
+        <li className="flex justify-between">
+          <p className="inline-flex max-w-[50%] text-left">Target:</p>
+          <p className="inline-flex max-w-[50%] text-right font-normal">
             {getDisplayName(moveData.target.name)}
           </p>
-        </div>
+        </li>
         {moveData.power && (
-          <div className="flex justify-between">
-            <p className="inline-flex max-w-[60%] text-left">Power:</p>
+          <li className="flex justify-between">
+            <p className="inline-flex max-w-[50%] text-left">Power:</p>
             <p className="inline-flex text-right font-normal">
               {moveData.power}
             </p>
-          </div>
+          </li>
         )}
         {Object.entries(moveData?.meta).map(
           (moveMeta) =>
             moveMeta[1] > 0 && (
-              <div className="flex justify-between">
-                <p className="inline-flex max-w-[60%] text-left">
+              <li className="flex justify-between">
+                <p className="inline-flex max-w-[50%] text-left">
                   {getDisplayName(moveMeta[0], '_')}:
                 </p>
-                <p className="inline-flex max-w-[40%] text-right font-normal">
+                <p className="inline-flex text-right font-normal">
                   {(moveMeta[1]?.name &&
                     getDisplayName(moveMeta[1]?.name, '+')) ||
                     moveMeta[1]}
                 </p>
-              </div>
+              </li>
             )
         )}
-      </div>
+      </ul>
     </div>
   );
 }
